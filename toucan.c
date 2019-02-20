66713b4904e808f08de600f075c64de860b3e229
Gediminas Simanskis <gediminas@rusoku.com>
2019-02-20 12:53:02 +0200

    small Copyright imrovement

--- toucan.c
+++ toucan.c
@@ -1,6 +1,8 @@
 /*
  * CAN driver for Rusoku Technologies TouCAN adapter
  *
+ * Copyright (C) 2019 Rusoku Technologies (info@rusoku.com)
+ *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published
  * by the Free Software Foundation; version 2 of the License.
